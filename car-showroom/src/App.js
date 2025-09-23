83d23bbded023384c997db6d1233236d87a3ccb1
Rodney_Karani <rodneykarani07@gmail.com>
2025-09-23 22:43:27 +0300

    Recreate Files & app

---
+++ car-showroom/src/App.js
@@ -0,0 +1,31 @@
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Cars from './pages/Cars';
+import CarDetails from './pages/CarDetails';
+import Showrooms from './pages/Showrooms';
+import AdminDashboard from './pages/AdminDashboard';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cars" element={<Cars />} />
+            <Route path="/cars/:id" element={<CarDetails />} />
+            <Route path="/showrooms" element={<Showrooms />} />
+            <Route path="/admin" element={<AdminDashboard />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
