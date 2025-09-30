daf548c9051565c0d8dc21145771250a15431d94
Benadette Mokandu <benadettemokandu@gmail.com>
2025-09-30 15:40:52 +0300

    Update start.sh for Render deployment: enable migrations and set FLASK_APP

--- Backend/start.sh
+++ Backend/start.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+set -o errexit
+
+# Set Flask app
+export FLASK_APP=app
+
 # Run database migrations
 flask db upgrade
 
-# Start Gunicorn server
-exec gunicorn app:app --bind 0.0.0.0:$PORT --workers 3
+# Start Gunicorn (production server)
+exec gunicorn -w 4 -b 0.0.0.0:$PORT app:app
